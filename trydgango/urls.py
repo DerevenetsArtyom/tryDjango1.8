ff9a4bb406e39367c9d2a0de727ab94a6681d63f
DerevenetsArtyom <fenix7036@gmail.com>
2016-03-20 20:19:39 +0200

    Add url for "accounts"

--- trydgango/urls.py
+++ trydgango/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     url(r'^$', 'newsletter.views.home', name='home'),
     url(r'^contact/$', 'newsletter.views.contact', name='contact'),
     url(r'^about/$', 'trydgango.views.about', name='about'),
-
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('registration.backends.default.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
